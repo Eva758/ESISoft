b8b5c0cf5dfb739db2fe720f0cdb74c6aa1c5a44
Francisco <Francisco@Francisco-PC>
2018-11-29 18:59:08 +0100

    Version 1.2

--- esisoft_core/src/main/java/esisoft/App.java
+++ esisoft_core/src/main/java/esisoft/App.java
@@ -16,7 +16,18 @@ import java.util.Scanner;
 public class App {
 	
 	public static void main(String[] args){
-		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					mesas = leerMesas("C:\\Users\\Propietario\\Desktop\\www\\esisoft\\src\\esisoft\\Mesas.txt");
+					visualizarMesas(mesas);
+					VentanaJefeSala frame = new VentanaJefeSala(mesas);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 			
 }
